7902a7a0144553b27037f4876804a737d1e2c9ca
floud <ahalturin84@gmail.com>
2016-02-29 14:18:23 +0300

    e

--- asyn_serv.py
+++ asyn_serv.py
@@ -1,28 +1,32 @@
 import asyncore
+import socket
 
 
 class EchoHandler(asyncore.dispatcher_with_send):
     def handle_read(self):
         data = self.recv(1024)
         if data == 'close':
-            print('closed by close data')
+            # print('closed by close data')
             self.close()
         elif data:
-            print('sending data to user..')
+            # print('sending data to user..')
             self.send(data)
 
 
 class EchoServer(asyncore.dispatcher):
     def __init__(self, host, port):
         asyncore.dispatcher.__init__(self)
-        self.create_socket()
+        self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
         self.set_reuse_addr()
         self.bind((host, port))
         self.listen(10)
 
-    def handle_accepted(self, sock, addr):
-        print('Incoming connection from %s' % repr(addr))
-        handler = EchoHandler(sock)
+    def handle_accepted(self):
+        pair = self.accept()
+        if pair is not None:
+            sock, addr = pair
+            print('Incoming connection from %s' % repr(addr))
+            handler = EchoHandler(sock)
 
 
 server = EchoServer('0.0.0.0', 2222)
